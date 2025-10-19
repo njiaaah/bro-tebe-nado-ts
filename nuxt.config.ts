a42020a65c583d4655901ba3445060d9b6f9face
njiaaah <njiah@njiah.ru>
2025-10-19 16:02:33 +0300

    im done with linting hopefully

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@pinia/nuxt'],
 
   devtools: {
-    enabled: true,
+    enabled: true
   },
 
   css: ['~/assets/css/main.css'],
 
   routeRules: {
-    '/': { prerender: true },
+    '/': { prerender: true }
   },
 
-  compatibilityDate: '2025-01-15',
-
-  eslint: {
-    config: {
-      extends: ['@nuxt/eslint-config'],
-      rules: {
-        'comma-dangle': 'off',
-        'brace-style': 'off',
-        semi: 'off',
-        '@typescript-eslint/semi': 'off',
-        '@typescript-eslint/no-extra-semi': 'off',
-        'no-extra-semi': 'off',
-        'semi-spacing': 'off',
-        quotes: 'off',
-        indent: 'off',
-        'arrow-spacing': 'off',
-        'prettier/prettier': 'error',
-      },
-    },
-  },
+  compatibilityDate: '2025-01-15'
 })
