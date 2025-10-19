735f95d12c8c7f19bb3e4ec16ed28b85bb001b81
njiaaah <njiah@njiah.ru>
2025-10-19 15:34:53 +0300

    update prettier config

--- app/stores/useTodoListStore.ts
+++ app/stores/useTodoListStore.ts
@@ -1,25 +1,24 @@
-import type { TodoItem } from "~~/types/todo.ts";
+import type { TodoItem } from '~~/types/todo.ts'
 
-export const useTodoListStore = defineStore("todoStore", () => {
-    const taskList = ref<TodoItem[]>([])
+export const useTodoListStore = defineStore('todoStore', () => {
+  const taskList = ref<TodoItem[]>([])
 
   const addTaskToList = (task: TodoItem) => {
-    taskList.value.push(task);
+    taskList.value.push(task)
   }
   const removeTaskFromList = (id: TodoItem['id']) => {
-    taskList.value = taskList.value.filter(task=> task.id !== id);
+    taskList.value = taskList.value.filter(task => task.id !== id)
   }
   const completeTask = (id: TodoItem['id']) => {
-    const task = taskList.value.find(task => task.id === id);
+    const task = taskList.value.find(task => task.id === id)
     if (task) {
-      task.completed = true;
+      task.completed = true
+    }
   }
-
-   };
   return {
     taskList,
     addTaskToList,
     removeTaskFromList,
     completeTask,
-  };
-});
+  }
+})
